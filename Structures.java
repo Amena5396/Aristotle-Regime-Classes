674bade856a8e0309a6341bca2e5c22cfa5ff017
Amena5396 <ajamali@udallas.edu>
2017-12-07 17:00:06 -0600

    First Class

---
+++ Structures.java
@@ -0,0 +1,32 @@
+package arAristotleRegimeClasses;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.FileReader;
+
+//import javax.json.Json;
+//import javax.json.JsonReader;
+//import javax.json.JsonParser;
+
+public class Structures {
+	
+	HashMap <String, ArrayList<Integer>> thestructure;
+		
+	public Structures() {
+		this.thestructure = new HashMap<String, ArrayList<Integer>>();
+	}
+	
+	//private void CountryData() {
+		//try {
+			
+			//JsonParser parser = Json.createParser(new StringReader("[]"));
+			
+			
+			
+		//} catch (IOException e) {
+			//e.printStackTrace();
+		//}
+//	}
+	
+}
+//}
